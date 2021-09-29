3c493c706f4d91c0ef4a0ba845ea6dd623bc4677
alisonroy <alisonroy12@gmail.com>
2021-09-29 14:57:44 +0530

    Test

--- src/views/Dashboard/GeneralInformation.js
+++ src/views/Dashboard/GeneralInformation.js
@@ -5,9 +5,7 @@ import {
   Table,
   Tbody,
   Text,
-  Th,
-  Thead,
-  Tr,
+  Input,
   useColorModeValue,
   FormControl,
   FormLabel,
@@ -28,74 +26,27 @@ function Academics() {
 
   return (
     <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
-      <Card mb="1rem">
-        <CardBody>
-          <Flex flexDirection="column" align="center" justify="center" w="100%">
-            <Text fontSize="xl" color={textColor} fontWeight="bold" mr="auto">
-              Academic Results
-            </Text>
-            <FormControl id="dept" display="inline" w="50%">
-              <FormLabel>Department</FormLabel>
-              <Select placeholder="Select Department">
-                <option>Computer Science Engineering (CSE)</option>
-                <option>Information technology (IT)</option>
-                <option>
-                  Electronics and Communications Engineering (ECE)
-                </option>
-                <option>Electrical & Electronics Engineering (EEE)</option>
-                <option>Mechanical Engineering A (MECH A)</option>
-                <option>Mechanical Engineering B (MECH B)</option>
-              </Select>
-            </FormControl>
-            <br />
-            <FormControl id="batch" display="inline" w="50%">
-              <FormLabel>Batch</FormLabel>
-              <Select placeholder="Select Batch">
-                <option>2022</option>
-                <option>2023</option>
-                <option>2024</option>
-                <option>2025</option>
-              </Select>
-            </FormControl>
-            <br />
-          </Flex>
-        </CardBody>
-      </Card>
-
       <Card overflowX={{ sm: "scroll", xl: "hidden" }}>
         <CardHeader p="6px 0px 22px 0px">
           <Text fontSize="xl" color={textColor} fontWeight="bold">
-            Students List
+            General Information
           </Text>
         </CardHeader>
         <CardBody>
-          <Table variant="simple" color={textColor}>
-            <Thead>
-              <Tr my=".8rem" pl="0px" color="gray.400">
-                <Th color="gray.400">S.No.</Th>
-                <Th pl="0px" color="gray.400">
-                  Name
-                </Th>
-                <Th color="gray.400">Resitration Number</Th>
-                <Th color="gray.400">Roll Number</Th>
-                <Th color="gray.400">Email</Th>
-                <Th></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {tablesTableData1.map((row) => {
-                return (
-                  <TablesTableRow
-                    sno={row.sno}
-                    name={row.name}
-                    reg={row.reg}
-                    email={row.email}
-                    roll={row.roll}
-                  />
-                );
-              })} 
-            </Tbody>
-          </Table>
+          <SimpleGrid columns={{ sm: 1, md: 2, xl: 2 }} spacing="24px" w="100%">
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+          </SimpleGrid>
         </CardBody>
       </Card>
     </Flex>
