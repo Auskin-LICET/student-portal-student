0ec3597d0a2be603f4b1a3d115fed4ccc603efd9
Sanjay03K <75990210+Sanjay03K@users.noreply.github.com>
2021-10-16 14:35:06 +0530

    Add files via upload

---
+++ src/routes1.js
@@ -0,0 +1,10 @@
+import GeneralInformationdata from "views/Pages/GeneralInformationdata";
+var NdashRoutes = [
+    {
+      path: "/GeneralInformationdata",
+      component: GeneralInformationdata,
+      layout: "/auth",
+    },
+  ];
+  export default NdashRoutes;
+  
