0ef3f90df009834573e8af1035ff91dfd92a27e8
Auskin26 <75899129+Auskin26@users.noreply.github.com>
2021-10-22 21:15:38 +0530

    ,

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -11,6 +11,7 @@ import {
 import PropTypes from "prop-types";
 import React, { useState } from "react";
 import AdminNavbarLinks from "./AdminNavbarLinks";
+import { Scrollbars } from "react-custom-scrollbars";
 
 export default function AdminNavbar(props) {
   const [scrolled, setScrolled] = useState(false);
@@ -70,93 +71,95 @@ export default function AdminNavbar(props) {
   };
   window.addEventListener("scroll", changeNavbar);
   return (
-    <Flex
-      position={navbarPosition}
-      boxShadow={navbarShadow}
-      bg={navbarBg}
-      borderColor={navbarBorder}
-      filter={navbarFilter}
-      backdropFilter={navbarBackdrop}
-      borderWidth="1.5px"
-      borderStyle="solid"
-      transitionDelay="0s, 0s, 0s, 0s"
-      transitionDuration=" 0.25s, 0.25s, 0.25s, 0s"
-      transition-property="box-shadow, background-color, filter, border"
-      transitionTimingFunction="linear, linear, linear, linear"
-      alignItems={{ xl: "center" }}
-      borderRadius="16px"
-      display="flex"
-      minH="75px"
-      justifyContent={{ xl: "center" }}
-      lineHeight="25.6px"
-      mx="auto"
-      mt={secondaryMargin}
-      pb="8px"
-      right={{ sm: "15px", xl: "30px" }}
-      px={{
-        sm: paddingX,
-        md: "30px",
-      }}
-      ps={{
-        xl: "12px",
-      }}
-      paddingTop="8px"
-      top="18px"
-      w={{ sm: "calc(100vw - 30px)", xl: "calc(100vw - 75px - 275px)" }}
-    >
+    <Scrollbars style={{ width: 500, height: 300 }}>
       <Flex
-        w="100%"
-        flexDirection={{
-          sm: "column",
-          md: "row",
-        }}
+        position={navbarPosition}
+        boxShadow={navbarShadow}
+        bg={navbarBg}
+        borderColor={navbarBorder}
+        filter={navbarFilter}
+        backdropFilter={navbarBackdrop}
+        borderWidth="1.5px"
+        borderStyle="solid"
+        transitionDelay="0s, 0s, 0s, 0s"
+        transitionDuration=" 0.25s, 0.25s, 0.25s, 0s"
+        transition-property="box-shadow, background-color, filter, border"
+        transitionTimingFunction="linear, linear, linear, linear"
         alignItems={{ xl: "center" }}
+        borderRadius="16px"
+        display="flex"
+        minH="75px"
+        justifyContent={{ xl: "center" }}
+        lineHeight="25.6px"
+        mx="auto"
+        mt={secondaryMargin}
+        pb="8px"
+        right={{ sm: "15px", xl: "30px" }}
+        px={{
+          sm: paddingX,
+          md: "30px",
+        }}
+        ps={{
+          xl: "12px",
+        }}
+        paddingTop="8px"
+        top="18px"
+        w={{ sm: "calc(100vw - 30px)", xl: "calc(100vw - 75px - 275px)" }}
       >
-        <Box mb={{ sm: "8px", md: "0px" }}>
-          <Breadcrumb>
-            <BreadcrumbItem color={mainText}>
-              <BreadcrumbLink href="#" color={secondaryText}>
-                Pages
-              </BreadcrumbLink>
-            </BreadcrumbItem>
+        <Flex
+          w="100%"
+          flexDirection={{
+            sm: "column",
+            md: "row",
+          }}
+          alignItems={{ xl: "center" }}
+        >
+          <Box mb={{ sm: "8px", md: "0px" }}>
+            <Breadcrumb>
+              <BreadcrumbItem color={mainText}>
+                <BreadcrumbLink href="#" color={secondaryText}>
+                  Pages
+                </BreadcrumbLink>
+              </BreadcrumbItem>
 
-            <BreadcrumbItem color={mainText}>
-              <BreadcrumbLink href="#" color={mainText}>
-                {brandText}
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-          </Breadcrumb>
-          {/* Here we create navbar brand, based on route name */}
-          <Link
-            color={mainText}
-            href="#"
-            bg="inherit"
-            borderRadius="inherit"
-            fontWeight="bold"
-            _hover={{ color: { mainText } }}
-            _active={{
-              bg: "inherit",
-              transform: "none",
-              borderColor: "transparent",
-            }}
-            _focus={{
-              boxShadow: "none",
-            }}
-          >
-            {brandText}
-          </Link>
-        </Box>
-        <Box ms="auto" w={{ sm: "100%", md: "unset" }}>
-          <AdminNavbarLinks
-            onOpen={props.onOpen}
-            rtlActive={rtlActive}
-            logoText={props.logoText}
-            secondary={props.secondary}
-            fixed={props.fixed}
-          />
-        </Box>
+              <BreadcrumbItem color={mainText}>
+                <BreadcrumbLink href="#" color={mainText}>
+                  {brandText}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </Breadcrumb>
+            {/* Here we create navbar brand, based on route name */}
+            <Link
+              color={mainText}
+              href="#"
+              bg="inherit"
+              borderRadius="inherit"
+              fontWeight="bold"
+              _hover={{ color: { mainText } }}
+              _active={{
+                bg: "inherit",
+                transform: "none",
+                borderColor: "transparent",
+              }}
+              _focus={{
+                boxShadow: "none",
+              }}
+            >
+              {brandText}
+            </Link>
+          </Box>
+          <Box ms="auto" w={{ sm: "100%", md: "unset" }}>
+            <AdminNavbarLinks
+              onOpen={props.onOpen}
+              rtlActive={rtlActive}
+              logoText={props.logoText}
+              secondary={props.secondary}
+              fixed={props.fixed}
+            />
+          </Box>
+        </Flex>
       </Flex>
-    </Flex>
+    </Scrollbars>
   );
 }
 
